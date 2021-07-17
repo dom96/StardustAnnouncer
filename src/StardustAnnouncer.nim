96d1289e5eefb98fde9c23fac9b1efe423dd55c7
Dominik Picheta <dominikpicheta@gmail.com>
2021-07-17 20:22:06 +0100

    Implements announcing live games.

--- src/StardustAnnouncer.nim
+++ src/StardustAnnouncer.nim
@@ -1,6 +1,22 @@
-import asyncdispatch, times, options, dimscord
+import asyncdispatch, times, options, logging, strformat, random, sequtils, sugar
 
+import dimscord
+
+import metrics
+
+type
+  State = ref object
+    lastActive: seq[string]
+    lastAnnouncement: float
+
+const announcementChannelId = "456504845642235914"
+const emotes = [
+  ":2716057:", ":VapeNaysh:", ":USA:", ":SpoopyBones:", ":SixTwoSix:",
+  ":PrideStar:", ":PogMaThoin:", ":Patrick:", ":OhMyGlob:", ":Ninja:",
+  ":BroFist:", ":BaconPancakes:"
+]
 let botToken = readFile("stardust-announcer.bot-token")
+
 let discord = newDiscordClient(botToken)
 
 # Handle event for on_ready.
@@ -11,27 +27,54 @@ proc onReady(s: Shard, r: Ready) {.event(discord).} =
 proc messageCreate(s: Shard, m: Message) {.event(discord).} =
   if m.author.bot: return
   if m.content == "!ping": # If message content is "!ping".
-    let
-      before = epochTime() * 1000
-      msg = await discord.api.sendMessage(m.channel_id, "ping?")
-      after = epochTime() * 1000
-    # Now edit the message.
-    # Use 'discard' because editMessage returns a new message.
-    discard await discord.api.editMessage(
-      m.channel_id,
-      msg.id,
-      "Pong! took " & $int(after - before) & "ms | " & $s.latency() & "ms."
-    )
-  elif m.content == "!embed": # Otherwise if message content is "!embed".
-    # Sends a message with embed.
-    discard await discord.api.sendMessage(
-      m.channel_id,
-      embed = some Embed(
-        title: some "Hello there!",
-        description: some "This is description",
-        color: some 0x7789ec
-      )
-    )
+    discard await discord.api.sendMessage(m.channel_id, "Pong!")
+    echo("Got pong from <#", m.channel_id, ">")
 
+proc sendAnnouncement(
+  state: State, servers: seq[string]
+) {.async.} =
+  if epochTime() - state.lastAnnouncement < 1800:
+    # Reset last announced servers after 30 minutes of no announcements.
+    state.lastActive = @[]
+  if servers.len == 0:
+    return
+  if state.lastActive == servers:
+    info("Not sending due to last active")
+    return
+  if epochTime() - state.lastAnnouncement < 300: # 5 minutes
+    info("Not sending due to last announcement")
+    return
+  # Create message.
+  for server in servers:
+    let uri = getPlayUrl(servers[0])
+    let name = getServerName(servers[0])
+    let emote = sample(emotes)
+    let msg = fmt"A few players are currently active in `{name}`." &
+      "Why not join them for a game?\n\n" &
+      fmt"Join via this link: <{uri}> {emote}"
+    discard await discord.api.sendMessage(announcementChannelId, msg)
+
+  state.lastAnnouncement = epochTime()
+  state.lastActive = servers
+
+proc runMetricsLoop() {.async.} =
+  var state = State(lastAnnouncement: epochTime())
+  while true:
+    await sleepAsync(5000)
+    try:
+      let metrics = await getMetrics()
+      debug("Got metrics: ", metrics)
+      await sendAnnouncement(state, getActiveServers(metrics))
+    except:
+      error("Unable to download metrics: ", getCurrentExceptionMsg())
+
+randomize()
+var consoleLog = newConsoleLogger()
+addHandler(consoleLog)
+# Set up metrics polling.
+let runMetricsLoopFut = runMetricsLoop()
+runMetricsLoopFut.callback =
+  proc (fut: Future[void]) =
+    error("Concurrent future failed with ", fut.error.msg)
 # Connect to Discord and run the bot.
 waitFor discord.startSession()
---
+++ src/metrics.nim
@@ -0,0 +1,72 @@
+import strutils, httpclient, parseutils, tables, asyncdispatch, uri
+
+type
+  Metrics* = object
+    playerCounts, botCounts: Table[string, int]
+
+proc parseMetrics(metrics: string): Metrics =
+  for line in metrics.splitLines():
+    if line.len == 0: continue
+    if line[0] == '#': continue
+    const playerCountsPrefix = "loadbalancer_server_player_count{uri="
+    const botCountsPrefix = "loadbalancer_server_bot_count{uri="
+    if line.startsWith(playerCountsPrefix):
+      let uri = line.captureBetween('"', start = playerCountsPrefix.len)
+      result.playerCounts[uri] = parseFloat(line.split("} ")[1]).int
+    if line.startsWith(botCountsPrefix):
+      let uri = line.captureBetween('"', start = botCountsPrefix.len)
+      result.botCounts[uri] = parseFloat(line.split("} ")[1]).int
+
+proc getMetrics*(): Future[Metrics] {.async.} =
+  var client = newAsyncHttpClient()
+  let metrics = await client.getContent("https://stardust.dev/route/metrics")
+  return parseMetrics(metrics)
+
+proc getPlayUrl*(uri: string): string =
+  var u = parseUri(uri)
+  u.scheme = ""
+  result = "https://stardust.dev/play/?ServerURI=" & $u
+  if result.endsWith("/"):
+    result = result[0 .. ^2]
+  if result.endsWith("/game"):
+    result = result[0 .. ^6]
+
+proc getServerName*(uri: string): string =
+  let u = parseUri(uri)
+  return u.hostname[0 .. 3]
+
+proc getActiveServers*(metrics: Metrics): seq[string] =
+  for key, val in metrics.playerCounts:
+    if val - metrics.botCounts.getOrDefault(key) > 0:
+      result.add(key)
+
+proc `$`*(metric: Metrics): string =
+  return system.`$`(metric)
+
+when isMainModule:
+  let metrics = """
+# HELP loadbalancer_server_player_count Number of players on each server.
+# TYPE loadbalancer_server_player_count gauge
+loadbalancer_server_player_count{uri="wss://fra1.stardust.dev/game2/"} 7.0
+loadbalancer_server_player_count{uri="wss://nyc1.stardust.dev/game2/"} 4.0
+loadbalancer_server_player_count 0.0
+# HELP loadbalancer_server_bot_count Number of bots on each server.
+# TYPE loadbalancer_server_bot_count gauge
+loadbalancer_server_bot_count{uri="wss://fra1.stardust.dev/game2/"} 7.0
+loadbalancer_server_bot_count{uri="wss://nyc1.stardust.dev/game2/"} 3.0
+loadbalancer_server_bot_count 0.0
+  """
+  let parsed = parseMetrics(metrics)
+  doAssert parsed.playerCounts["wss://fra1.stardust.dev/game2/"] == 7
+  doAssert parsed.playerCounts["wss://nyc1.stardust.dev/game2/"] == 4
+  doAssert parsed.botCounts["wss://fra1.stardust.dev/game2/"] == 7
+  doAssert parsed.botCounts["wss://nyc1.stardust.dev/game2/"] == 3
+  echo parsed
+
+  doAssert(getPlayUrl("wss://nyc1.stardust.dev/game2/") == "https://stardust.dev/play/?ServerURI=nyc1.stardust.dev/game2")
+  doAssert(getPlayUrl("wss://nyc1.stardust.dev/game/") == "https://stardust.dev/play/?ServerURI=nyc1.stardust.dev")
+
+  doAssert(getServerName("wss://nyc1.stardust.dev/game2/") == "nyc1")
+
+  doAssert(getActiveServers(parsed) == @["wss://nyc1.stardust.dev/game2/"])
+  echo "All Good"
